b4326942aaaac869ddd7621d5897766636d1ac87
Melissa Skevington <mskeving@gmail.com>
2014-10-09 13:42:35 -0700

    geolocation added

--- app/static/js/parking.js
+++ app/static/js/parking.js
@@ -1,20 +1,35 @@
 $(document).ready(function() {
-    function initialize() {
-        var myLatlng = new google.maps.LatLng(37.736248,-122.448935);
+    function initializeMap() {
+        var mapElm = document.getElementById('map-canvas');
         var mapOptions = {
-            center: myLatlng,
             zoom: 17
         };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
+        var map = new google.maps.Map(mapElm, mapOptions);
 
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map
-        });
+        //HTML geolocation
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = new google.maps.LatLng(position.coords.latitude,
+                                                position.coords.longitude);
+                var marker = new google.maps.Marker({
+                    map: map, 
+                    draggable: true,
+                    position: pos
+                });
+
+                map.setCenter(pos);
+            }, function() {
+                handleNoGeolocation(true, map);
+            });
+
+        } else {
+            // browser doesn't support geolocation
+            handleNoGeolocation(false, map);
+        }
+
     }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, 'load', initializeMap);
 
     $('#change-loc-butt').on('click', function() {
         document.location.href = '/park';
@@ -42,3 +57,34 @@ $(document).ready(function() {
 
     })
 });
+
+function handleNoGeolocation(errorFlag, map) {
+    if (errorFlag) {
+        var content = 'Error: Geolocation failed.';
+    } else {
+        var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+        map: map,
+        position: new google.maps.LatLng(60,105),
+        content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+}
+
+function detectBrowser() {
+    console.log('ys');
+    var useragent = navigator.userAgent;
+    var mapdiv = document.getElementById("map-canvas");
+  
+    if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+        mapdiv.style.width = '100%';
+        mapdiv.style.height = '100%';
+    } else {
+        mapdiv.style.width = '600px';
+        mapdiv.style.height = '800px';
+    }
+}
